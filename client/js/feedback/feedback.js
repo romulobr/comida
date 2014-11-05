424cdeda1be235886ef6f868edebfcdcf2fda637
Romulo Santos <romulobr@gmail.com>
2014-11-05 01:28:04 -0200

    integrates feedback message to backend

--- client/js/feedback/feedback.js
+++ client/js/feedback/feedback.js
@@ -1,9 +1,28 @@
 var feedback = angular.module('feedback', ['ngRoute', 'lbServices', 'ngMaterial']);
 
-feedback.controller('SendFeedbackController', function ($scope, $http) {
+feedback.controller('SendFeedbackController', function ($scope, $http, Feedback, $materialDialog) {
     $scope.contact = {};
+
+    $scope.dialog = function (e, orderItem) {
+        $materialDialog({
+            templateUrl: 'js/feedback/thanks.html',
+            targetEvent: e,
+            controller: ['$scope', '$hideDialog', function ($scope, $hideDialog) {
+                $scope.close = function () {
+                    $hideDialog();
+                };
+            }]
+        });
+    }
+
     $scope.send = function () {
         if($scope.contact.message !== '')
-        $http.post('/feedback/', $scope.contact);
+            Feedback.upsert($scope.contact, function (feedback) {
+                $scope.contact = feedback;
+                $scope.dialog();
+            }, function (error) {
+                console.log('error sending feedback:');
+                console.log(error);
+            });
     }
 });
