dabea73010be921f2e802f4d10c8afb1fe6057b1
Carlos Villela <cvillela@thoughtworks.com>
2014-12-08 21:51:33 -0200

    idempotent installs, took out sudo

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-sudo apt-get -y install git 
-cd /vagrant
-sudo npm install -g bower
-npm install
-cd client
-bower install
-cd ..
-nodejs server/server.js&
+set -e
+set -o pipefail
+set -x
+
+pushd /vagrant
+
+  if [ -f .needs-update ]; then
+    apt-get -y update
+    apt-get -y upgrade
+    rm -rf .needs-update
+  fi
+
+  which git > /dev/null || apt-get -y install git
+
+  npm install -s
+  pushd client
+    ../node_modules/bower/bin/bower --allow-root install
+  popd
+
+popd
